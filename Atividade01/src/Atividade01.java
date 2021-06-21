0189de62f91b9484107c50c596b79959d857712d
Cláudio Morais <claudio_blz@yahoo.com.br>
2021-06-20 23:19:03 -0300

    Java version update to 1.8

---
+++ Atividade01/src/Atividade01.java
@@ -0,0 +1,17 @@
+import java.math.BigDecimal;
+
+public class Atividade01 {
+
+    private String nomeFuncionário;
+    private BigDecimal salario;
+    private Integer total;
+
+
+
+
+    public static void main(String[] args) {
+
+        Atividade01 atividade01 = new Atividade01();
+        System.out.println("Ola mundo");
+    }
+}
