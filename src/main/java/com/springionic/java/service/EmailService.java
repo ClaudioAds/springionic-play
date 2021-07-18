fcb040ebd8e072f60bde09f79ecf6d5d48d07a07
ClaudioAds <claudio.blz25@gmail.com>
2021-07-18 02:48:07 -0300

    Revert "HTML Email send created sucess"
    This reverts commit 1db92fef640c254f289a97bbdbf463f726366bd2.

--- src/main/java/com/springionic/java/service/EmailService.java
+++ src/main/java/com/springionic/java/service/EmailService.java
@@ -3,15 +3,9 @@ package com.springionic.java.service;
 import com.springionic.java.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
-import javax.mail.internet.MimeMessage;
-
 public interface EmailService {
-//========= Versão para texto plano
+
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage obj);
-//=========== Versão para texto html
-    void sendOrderConfirmationHtmlEmail(Pedido obj);
-
-    void sendHtmlEmail(MimeMessage msg);
 }
