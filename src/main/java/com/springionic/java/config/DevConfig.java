b1c751086d744dc6a6babe2fef19a0e3fa2aa731
ClaudioAds <claudio.blz25@gmail.com>
2021-07-17 21:16:33 -0300

    SmtpService implements with google server

--- src/main/java/com/springionic/java/config/DevConfig.java
+++ src/main/java/com/springionic/java/config/DevConfig.java
@@ -1,6 +1,8 @@
 package com.springionic.java.config;
 
 import com.springionic.java.service.DBService;
+import com.springionic.java.service.EmailService;
+import com.springionic.java.service.SmtpEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -26,5 +28,10 @@ public class DevConfig {
 
         dbService.instantiateTestDatabase();
         return true;
+    }
+
+    @Bean
+    public EmailService emailService() {
+        return new SmtpEmailService();
     }
 }
