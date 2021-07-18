a7d2335c007d316cba705fb7ce29c1c421d3f0bd
ClaudioAds <claudio.blz25@gmail.com>
2021-07-18 02:54:19 -0300

    Revert "Revert "HTML Email send created sucess""
    This reverts commit fcb040ebd8e072f60bde09f79ecf6d5d48d07a07.

--- src/main/java/com/springionic/java/service/SmtpEmailService.java
+++ src/main/java/com/springionic/java/service/SmtpEmailService.java
@@ -5,17 +5,30 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import javax.mail.internet.MimeMessage;
 
 public class SmtpEmailService extends AbstractEmailService{
 
     @Autowired
     private MailSender mailSender;//responsável por pegar todos os dados de email do file aplication-dev.properties
 
+    @Autowired
+    private JavaMailSender javaMailSender;//JavaMailSender que é capaz de enviar o MimeMessage
+
     private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
     @Override
     public void sendEmail(SimpleMailMessage msg) {
         LOG.info("Enviando Email....");
         mailSender.send(msg);
+        LOG.info("Email enviado!");
+    }
+
+    @Override
+    public void sendHtmlEmail(MimeMessage msg) {
+        LOG.info("Enviando Email HTML....");
+        javaMailSender.send(msg);
         LOG.info("Email enviado!");
     }
 }
