b59568eef22145eab25675b94b41ac9ae579cdac
ClaudioAds <claudio.blz25@gmail.com>
2021-09-29 18:38:15 -0300

    first file upload on S3 AWS

--- src/main/java/com/springionic/java/SpringIonicApplication.java
+++ src/main/java/com/springionic/java/SpringIonicApplication.java
@@ -1,38 +1,16 @@
 package com.springionic.java;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
+import com.springionic.java.service.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.springionic.java.domain.Categoria;
-import com.springionic.java.domain.Cidade;
-import com.springionic.java.domain.Cliente;
-import com.springionic.java.domain.Endereco;
-import com.springionic.java.domain.Estado;
-import com.springionic.java.domain.ItemPedido;
-import com.springionic.java.domain.Pagamento;
-import com.springionic.java.domain.PagamentoComBoleto;
-import com.springionic.java.domain.PagamentoComCartao;
-import com.springionic.java.domain.Pedido;
-import com.springionic.java.domain.Produto;
-import com.springionic.java.domain.enums.EstadoPagamento;
-import com.springionic.java.domain.enums.TipoCliente;
-import com.springionic.java.repository.CategoriaRepository;
-import com.springionic.java.repository.CidadeRepository;
-import com.springionic.java.repository.ClienteRepository;
-import com.springionic.java.repository.EnderecoRepository;
-import com.springionic.java.repository.EstadoRepository;
-import com.springionic.java.repository.ItemPedidoRepository;
-import com.springionic.java.repository.PagamentoRepository;
-import com.springionic.java.repository.PedidoRepository;
-import com.springionic.java.repository.ProdutoRepository;
-
 @SpringBootApplication
 public class SpringIonicApplication implements CommandLineRunner{
+
+    @Autowired
+    private S3Service service;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringIonicApplication.class, args);
@@ -40,7 +18,7 @@ public class SpringIonicApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		service.uploadFile("/home/dev/Imagens/pp.jpeg");
 	}
 
 }
